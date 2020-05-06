32b416bbb34c409d144c25ca8822b8716affc8fa
Jiayu Wang <mail@wangjiayu.net>
2020-05-06 15:43:48 -0500

    First Commit

---
+++ src/build/precache-manifest.f63b091602d25cfb8915340bd502f5ea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71c5cabef906435a75ec368b0c08760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fda90862df70bca5c235",
+    "url": "/static/css/main.4267c396.chunk.css"
+  },
+  {
+    "revision": "0db463148ac9f5de8bfb",
+    "url": "/static/js/2.c7764897.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c7764897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda90862df70bca5c235",
+    "url": "/static/js/main.e8ebe86d.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "ec56c8e9b19a00590fc8379efae8c34a",
+    "url": "/static/media/nginx-1.ec56c8e9.svg"
+  },
+  {
+    "revision": "3da67b7657fedcdded70bdf08bc9d08a",
+    "url": "/static/media/raspberry-pi.3da67b76.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/static/media/react.4009eef3.svg"
+  }
+]);
